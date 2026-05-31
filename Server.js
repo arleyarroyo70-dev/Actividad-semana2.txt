ea37513519336432d86a5bbe62902e37d95c737c
Arley Arroyo <arleyarroyo70@gmail.com>
2026-05-31 21:31:09 +0000

    Actividad semana 4

---
+++ Server.js
@@ -0,0 +1,4 @@
+const express=require ('express');
+const app=espless();
+app.get('/'‚ (req, res)=>{res.send ('servidor funcionando correctamente '); });
+app. listen(3000,()=>{console.log('servidor ejecutándose en });
